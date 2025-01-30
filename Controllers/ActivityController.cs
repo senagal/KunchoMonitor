ae71d3e640c12519cf96ee11cb09612385dc3d21
selamalex <121111132+selamalex@users.noreply.github.com>
2025-01-30 10:58:47 +0300

    Refined XML comments for ActivityController to generate XML documentation

--- Controllers/ActivityController.cs
+++ Controllers/ActivityController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 namespace activity.Controllers
 {
     /// <summary>
-    /// Controller responsible for managing activities related to content.
+    /// Provides API endpoints for managing activities associated with content.
     /// </summary>
     [Route("api/[controller]")]
     [ApiController]
@@ -16,20 +16,24 @@ namespace activity.Controllers
         private readonly IActivityServices _activityService;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ActivityController"/> class.
+        /// Initializes a new instance of the <see cref="ActivityController"/> class with the specified activity service.
         /// </summary>
-        /// <param name="activityService">The service used to manage activities.</param>
+        /// <param name="activityService">The service used to manage and retrieve activities.</param>
         public ActivityController(IActivityServices activityService)
         {
             _activityService = activityService;
         }
 
-
         /// <summary>
-        /// Retrieves the activity associated with the specified content ID.
+        /// Retrieves the list of activities associated with the specified content ID.
         /// </summary>
         /// <param name="contentId">The unique identifier of the content.</param>
-        /// <returns>The activity associated with the given content ID, or NotFound if it doesn't exist.</returns>
+        /// <returns>
+        /// An <see cref="ActionResult"/> containing a list of activities associated with the given content ID 
+        /// if they exist; otherwise, a <see cref="NotFoundResult"/> indicating no activities were found.
+        /// </returns>
+        /// <response code="200">Returns the list of activities for the specified content ID.</response>
+        /// <response code="404">Indicates no activities were found for the specified content ID.</response>
         [HttpGet("{contentId}")]
         public async Task<ActionResult<List<Activity>>> GetActivitiesByContentId(int contentId)
         {
